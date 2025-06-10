4c82a1bc9a5708b8ad94d1f7c24ad54c874096f9
SuchitHubale <2217043@ritindia.edu>
2025-06-10 20:26:04 +0530

    Update loading page

--- components/loading.tsx
+++ components/loading.tsx
@@ -3,43 +3,90 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center" role="status" aria-label="Loading">
-      <div className="loader">
-        <style jsx>{`
-          .loader {
-            width: 48px;
-            height: 48px;
-            background: #FFF;
-            border-radius: 50%;
-            display: inline-block;
-            position: relative;
-            box-sizing: border-box;
-            animation: rotation 1s linear infinite;
-          }
-          .loader::after {
-            content: '';  
-            box-sizing: border-box;
-            position: absolute;
-            left: 6px;
-            top: 10px;
-            width: 12px;
-            height: 12px;
-            color:rgb(141, 42, 194); /* Using blue-500 to match the website theme */
-            background: currentColor;
-            border-radius: 50%;
-            box-shadow: 25px 2px, 10px 22px;
-          }
-
-          @keyframes rotation {
-            0% {
-              transform: rotate(0deg);
-            }
-            100% {
-              transform: rotate(360deg);
-            }
-          }
-        `}</style>
+    <div className="min-h-screen flex items-center justify-center -mt-32" role="status" aria-label="Loading">
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1"></div>
+        <div className="sk-cube sk-cube2"></div>
+        <div className="sk-cube sk-cube3"></div>
+        <div className="sk-cube sk-cube4"></div>
+        <div className="sk-cube sk-cube5"></div>
+        <div className="sk-cube sk-cube6"></div>
+        <div className="sk-cube sk-cube7"></div>
+        <div className="sk-cube sk-cube8"></div>
+        <div className="sk-cube sk-cube9"></div>
       </div>
+      <style jsx>{`
+        .sk-cube-grid {
+          width: 40px;
+          height: 40px;
+          margin: 100px auto;
+        }
+
+        .sk-cube-grid .sk-cube {
+          width: 33%;
+          height: 33%;
+          background-color: #8d2ac2;
+          float: left;
+          -webkit-animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out;
+          animation: sk-cubeGridScaleDelay 1.3s infinite ease-in-out; 
+        }
+        .sk-cube-grid .sk-cube1 {
+          -webkit-animation-delay: 0.2s;
+          animation-delay: 0.2s; 
+        }
+        .sk-cube-grid .sk-cube2 {
+          -webkit-animation-delay: 0.3s;
+          animation-delay: 0.3s; 
+        }
+        .sk-cube-grid .sk-cube3 {
+          -webkit-animation-delay: 0.4s;
+          animation-delay: 0.4s; 
+        }
+        .sk-cube-grid .sk-cube4 {
+          -webkit-animation-delay: 0.1s;
+          animation-delay: 0.1s; 
+        }
+        .sk-cube-grid .sk-cube5 {
+          -webkit-animation-delay: 0.2s;
+          animation-delay: 0.2s; 
+        }
+        .sk-cube-grid .sk-cube6 {
+          -webkit-animation-delay: 0.3s;
+          animation-delay: 0.3s; 
+        }
+        .sk-cube-grid .sk-cube7 {
+          -webkit-animation-delay: 0s;
+          animation-delay: 0s; 
+        }
+        .sk-cube-grid .sk-cube8 {
+          -webkit-animation-delay: 0.1s;
+          animation-delay: 0.1s; 
+        }
+        .sk-cube-grid .sk-cube9 {
+          -webkit-animation-delay: 0.2s;
+          animation-delay: 0.2s; 
+        }
+
+        @-webkit-keyframes sk-cubeGridScaleDelay {
+          0%, 70%, 100% {
+            -webkit-transform: scale3D(1, 1, 1);
+            transform: scale3D(1, 1, 1);
+          } 35% {
+            -webkit-transform: scale3D(0, 0, 1);
+            transform: scale3D(0, 0, 1); 
+          }
+        }
+
+        @keyframes sk-cubeGridScaleDelay {
+          0%, 70%, 100% {
+            -webkit-transform: scale3D(1, 1, 1);
+            transform: scale3D(1, 1, 1);
+          } 35% {
+            -webkit-transform: scale3D(0, 0, 1);
+            transform: scale3D(0, 0, 1);
+          } 
+        }
+      `}</style>
     </div>
   )
 }
